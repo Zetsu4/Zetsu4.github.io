1cb688c1012152ecc9f16c3ee5834733ef4e6c7b
Zetsu4 <42389794+Zetsu4@users.noreply.github.com>
2018-09-12 14:04:36 -0600

    added interactive scene

--- mouseMovingWidth/sketch.js
+++ mouseMovingWidth/sketch.js
@@ -6,7 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
 }
 
 function keyTyped() {
