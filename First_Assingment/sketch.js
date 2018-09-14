0934e60275adf525248512615bbef49814490256
Zetsu4 <42389794+Zetsu4@users.noreply.github.com>
2018-09-13 21:06:31 -0600

    Worked on Pong With Friends

--- First_Assingment/sketch.js
+++ First_Assingment/sketch.js
@@ -1,41 +1,36 @@
 // First_Assignment (Pong with friends)
 // Travis Ahern
-// Sept. 13/2018
+// Sept. 13, 2018
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-//global variables
-let playerY_1 = false;
-let playerY_2 = false;
-let speed = 20;
-let compY_1 = false;
-let compY_2 = false;
+//creating global variables
+let playerYPos_1 = widowHeight/2;
+let playerYPos_2 = widowHeight/2;
+let paddleSpeed = windowHeight/20;
+let compYPos_1 = widowHeight/2;
+let compYPos_2 = widowHeight/2;
+let paddleLeftPosition = 50;
+let paddleRightPosition = windowWidth - 50;
+let paddleWidth = 20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(255);
+  background(0);
+  //player 1's paddle
+  rect(mouseX, mouseY, paddleWidth, 20);
 }
 
-function mousePressed() {
-  if (keyIsPressed) {
-    if (key === "r") {
-      rect(mouseX, mouseY, 10, 10);
-    }
-  	if (key === "e") {
-      ellipse(mouseX, mouseY, 10, 10);
-    }
+function keyIsPressed() {
+  //player 1's movement
+  if (key === "w" && playerYPos_1 < 0) {
+    playerYPos_1 += paddleSpeed;
   }
-}
-
-function keyTyped() {
-  if (key === "w") {
-    background(255);
-  }
-  else if (key === "b") {
-    background(0);
+	if (key === "s" && (playerYPos_1 + height/10) > height) {
+    playerYPos_1 -= paddleSpeed;
   }
 }
