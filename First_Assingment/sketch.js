ad2ebb53d9cc7ea6decaedaaffdb0ab8960d67c4
Zetsu4 <42389794+Zetsu4@users.noreply.github.com>
2018-09-14 14:09:26 -0600

    Worked on Pong With Friends

--- First_Assingment/sketch.js
+++ First_Assingment/sketch.js
@@ -8,12 +8,17 @@
 //creating global variables
 let playerYPos_1 = 400;
 let playerYPos_2 = 400;
-let paddleSpeed = 20;
+let paddleSpeed_player = 5;
+let paddleSpeed_comp = 1;
 let compYPos_1 = 400;
 let compYPos_2 = 400;
 let paddleLeftPosition = 50;
-let paddleRightPosition = 500;
+let paddleRightPosition = 1500;
 let paddleWidth = 20;
+let paddleHeight = 50;
+let ballX = 775;
+let ballY = 370;
+let ballSize = 30;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -21,16 +26,32 @@ function setup() {
 
 function draw() {
   background(0);
-  //player 1's paddle
-  rect(paddleLeftPosition, playerYPos_1, paddleWidth, 100);
-}
 
-function keyIsPressed() {
-  //player 1's movement
-  if (keyIsDown(87) && playerYPos_1 > 0) {
-    playerYPos_1 -= paddleSpeed;
+  //the ball
+  ellipse(ballX, ballY, ballSize);
+
+  //player 1's paddle
+  rect(paddleLeftPosition, playerYPos_1, paddleWidth, paddleHeight);
+
+  //computer paddle
+  rect(paddleRightPosition, compYPos_1, paddleWidth, paddleHeight);
+
+  //computer's movement
+  if ( (compYPos_1 + paddleHeight/2) > ballY) {
+    compYPos_1 -= paddleSpeed_comp;
   }
-	if (keyIsDown(83) && playerYPos_1 < 1000) {
-    playerYPos_1 += paddleSpeed;
+  if ( (compYPos_1 + paddleHeight/2) < ballY) {
+    compYPos_1 += paddleSpeed_comp;
+  }
+
+  //player's movement
+  if (keyIsPressed) {
+    //player 1's movement
+    if (keyIsDown(87) && playerYPos_1 > 0) {
+      playerYPos_1 -= paddleSpeed_player;
+    }
+  	if (keyIsDown(83) && playerYPos_1 < 1000) {
+      playerYPos_1 += paddleSpeed_player;
+    }
   }
 }
