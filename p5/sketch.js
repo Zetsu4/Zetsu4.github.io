590da1b20c8bdb604c0599fc1c311f2233f54d00
Zetsu4 <42389794+Zetsu4@users.noreply.github.com>
2018-09-25 12:10:23 -0600

    Created new folder

---
+++ p5/sketch.js
@@ -0,0 +1,14 @@
+// Project Title
+// Travis Ahern
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+}
