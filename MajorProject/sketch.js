cfdbc94bbe03694cc5901afb5ffb40edcdf20fd7
Zetsu4 <42389794+Zetsu4@users.noreply.github.com>
2018-09-28 14:11:30 -0600

    Did a thing

--- MajorProject/sketch.js
+++ MajorProject/sketch.js
@@ -1,14 +1,19 @@
-// Project Title
+// Major Project (progressive V1.0)
 // Travis Ahern
-// Date
+// Sept. 26/2018
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0);
 }
-
+//Dungeon Crwaler
+//
 function draw() {
 
 }
