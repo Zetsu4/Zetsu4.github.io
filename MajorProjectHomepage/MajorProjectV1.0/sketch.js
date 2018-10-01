e1ec4b362d7791be796af99e9c5b44e9404d8dc4
Zetsu4 <42389794+Zetsu4@users.noreply.github.com>
2018-09-30 19:17:00 -0600

    Worked on the next project

--- MajorProjectHomepage/MajorProjectV1.0/sketch.js
+++ MajorProjectHomepage/MajorProjectV1.0/sketch.js
@@ -5,9 +5,14 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let state = 1;
 let homePage;
-let allRaces;
+let allRaces, allCharClasses;
 let boxChoiceX, boxChoiceY;
+let playerRace = 0;
+let playerClass = 0;
+let playerHasChosenRace = 0;
+let playerHasChosenClass = 0;
 
 function preload() {
   homePage = loadImage("assets/lovelyHomepage.png");
@@ -15,27 +20,173 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  allRaces = [5, 1, 5, 2, 6];
+  textFont("Font Style Bold", height*0.05);
 
-  boxChoiceX = width*0.06;
+  allRaces = ["Random", "Human", "Half-Elf", "Elf", "Dwarf", "Halfling", "Goblins", "Orcs", "Uruk-Hai"];
+  allCharClasses = ["Random", "Archer", "Ranger", "Fighter", "Samurai", "Mage", "Cleric", "Rogue", "Enemy"];
+
+  boxChoiceX = width*0.30;
   boxChoiceY = height*0.10;
 }
 
+//------------------------------------------------------------------------------
+// CREATING A CHARACTER     START
+//------------------------------------------------------------------------------
+
+// RACE
+
+// drawing the race options
 function raceGroup(x, y, w, h) {
   rect(x, y, w, h);
   for (let race = 0; race < allRaces.length; race++) {
-    fill(0, 255, 0);
-    rect(x + race*boxChoiceX, y + race*boxChoiceY, boxChoiceX, boxChoiceY);
+    rect(x, y + race*boxChoiceY, boxChoiceX, boxChoiceY);
+    fill("black");
+    text(allRaces[race], x, y + race*boxChoiceY + height*0.05);
+    fill(255, 0, 0);
   }
 }
 
-function classGroup(x, y, w, h) {
+// highlighting players choice for their Race
+function highlightRace(x, y, w, h, whichRace) {
+  fill(0, 255, 0);
+  rect(x, y + whichRace*h, w, h);
+  fill("black");
+  text(allRaces[whichRace], x, y + whichRace*h + height*0.05);
+  fill(255, 0, 0);
+}
+
+// creating the race sprite
+function displayRace() {
+
+}
+
+// letting player select a race
+function chooseRace(x, y, w, h) {
+  if (mouseIsPressed && mouseX >= x && mouseX <= x + w) {
+    for (let choices = 0; choices < allRaces.length; choices++) {
+      if (mouseY >= y + h*choices && mouseY <= y + h*(choices + 1)) {
+        playerRace = allRaces[choices];
+        playerHasChosenRace = choices;
+      }
+    }
+  }
+  highlightRace(x, y, w, h, playerHasChosenRace);
+  displayRace(playerHasChosenRace);
+}
+
+// CLASS
+
+// drawing the class options
+function charClassGroup(x, y, w, h) {
   rect(x, y, w, h);
+  for (let charClass = 0; charClass < allCharClasses.length; charClass++) {
+    rect(x, y + charClass*boxChoiceY, boxChoiceX, boxChoiceY);
+    fill("black");
+    text(allCharClasses[charClass], x, y + charClass*boxChoiceY + height*0.05);
+    fill(255, 0, 0);
+  }
+}
+
+// highlighting players choice for their Class
+function highlightClass(x, y, w, h, whichClass) {
+  fill(0, 255, 0);
+  rect(x, y + whichClass*h, w, h);
+  fill("black");
+  text(allCharClasses[whichClass], x, y + whichClass*h + height*0.05);
+  fill(255, 0, 0);
+}
+
+// creating the class accessories
+function displayClass() {
+
+}
+
+// letting player slect a class
+function chooseCharClass(x, y, w, h) {
+  if (mouseIsPressed && mouseX >= x && mouseX <= x + w) {
+    for (let choices = 0; choices < allCharClasses.length; choices++) {
+      if (mouseY >= y + h*choices && mouseY <= y + h*(choices + 1)) {
+        playerClass = allCharClasses[choices];
+        playerHasChosenClass = choices;
+      }
+    }
+  }
+  highlightClass(x, y, w, h, playerHasChosenClass);
+  displayClass();
+}
+
+// CONT BUTTON
+
+// when the player has decided their race and class
+function continueButton(x, y, w, h) {
+  rect(x, y, w, h);
+  fill("black");
+  text("CONTINUE", x, y*2);
+  fill(255, 0, 0);
+  if (mouseIsPressed && mouseX >= x && mouseX <= x + w && mouseY >= y && mouseY <= y + h) {
+    state = 2;
+  }
+}
+
+//------------------------------------------------------------------------------
+// CREATING A CHARACTER     END
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// PLAYING OR SOMETHING     START
+//------------------------------------------------------------------------------
+
+// // display settings menu
+// function settingsMenu() {
+//
+// }
+//
+// // the minimap
+// function miniMap() {
+//
+// }
+
+// player buttons
+function playerActions() {
+
+}
+
+function enemy() {
+  
+}
+
+//------------------------------------------------------------------------------
+// PLAYING OR SOMETHING     END
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// WHERE THE GAME TAKES PLACE     START
+//------------------------------------------------------------------------------
+
+// checking what state the game is in
+function checkState() {
+  if (state === 1) {
+    // CREATING A CHARACTER
+    background(50);
+    raceGroup(width*0.05, height*0.05, boxChoiceX, height*0.90);
+    charClassGroup(width*0.65, height*0.05, boxChoiceX, height*0.90);
+    chooseRace(width*0.05, height*0.05, boxChoiceX, boxChoiceY);
+    chooseCharClass(width*0.65, height*0.05, boxChoiceX, boxChoiceY);
+    continueButton(width*0.425, height*0.05, width*0.15, height*0.05);
+  }
+  if (state === 2) {
+    background(0, 0, 255);
+    // settingsMenu();
+    // miniMap();
+    playerActions();
+    enemy();
+  }
 }
 
 function draw() {
-  background(0);
-  fill(255, 0, 0);
-  raceGroup(width*0.05, height*0.05, width*0.30, height*0.90);
-  classGroup(width*0.65, height*0.05, width*0.30, height*0.90);
+  checkState();
 }
+
+//------------------------------------------------------------------------------
+// WHERE THE GAME TAKES PLACE     END
+//------------------------------------------------------------------------------
--- MajorProjectHomepage/sketch.js
+++ MajorProjectHomepage/sketch.js
@@ -1,9 +1,6 @@
 // Major Project (progressive)
 // Travis Ahern
 // Sept. 26/2018
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,9 +10,37 @@ function draw() {
 
 }
 
-//RPG similar to LOTRO and Tale of Toast
-//choose class and race
-//race starts you at different regions
-//designing characters (poorly)
-//minimap function
-//lvl based
+// RPG similar to LOTRO and Tale of Toast
+// choose class and race
+// race starts you at different regions
+// designing characters (poorly)
+// minimap function
+// lvl based
+// guild that you can recruit for a party
+
+// stats:
+// Intellegence - magic
+// Agillity - movement speed and dodge rating
+// Strength - damage and attack rating with melee wepeons
+// Dexterity - damage and attack rating with ranged wepeons
+// Vitality - health and regeneration
+
+// stuff about the races:
+// Humans - normal boring humans, balanced and crappy
+// Half-Elf - more agillity, dexterity and a greater base states
+// Elf - more intellegent, agillity, dexterity and the rest balanced
+// Dwarves - more strength and vitality, less agility and intellegence
+// Goblins - a lot more agillity and dexterity, a little more strength and a lot less vitality, no intellegence
+// Orcs - a lot more agillity and dexterity a little more strength but a lot less intelligence and vitality
+// Urak-Hai - a lot more strength, dexterity, vitality, but little agility and no intellegence,
+
+
+// charClasses:
+// Fighter - melee, str vit
+// Ranger - melee and ranged and magic, str dex int
+// Archer - ranged, dex agi
+// Samurai - melee and magic both offensive and deffensive, str int vit
+// Mage - attack magic, int
+// Cleric - healing and support magic, int
+// Rogue - melee and magic both offensive and deffensive, agi int
+// Enemy - all the abilaties
