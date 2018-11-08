15bcedbeb29d02f1dea345ab0827764ae384eb2c
Zetsu4 <42389794+Zetsu4@users.noreply.github.com>
2018-11-08 14:12:10 -0600

    f

---
+++ MajorProjectHomepage/MajorProjectV4.0/assets/Objects/healthPotion.js
@@ -0,0 +1,66 @@
+class hpPotion {
+  constructor(img, itemInWorldX, itemInWorldY, itemMapX, itemMapY) {
+    // sprite
+    this.img = img;
+
+    // item changing positions
+    this.itemX = itemInWorldX;
+    this.itemY = itemInWorldY;
+
+    // item positions on map
+    this.mapX = itemMapX;
+    this.mapY = itemMapY;
+  }
+
+  pickUp(spriteW, spriteH) {
+    return this.itemX + width/2 >= width/2 - spriteW/2 && this.itemX + width/2 <= width/2 + spriteW/2
+        && this.itemY + height/2 >= height/2 - spriteH/2 && this.itemY + height/2 <= height/2 + spriteH/2;
+  }
+
+  moveWithPlayerX(keyLeft, keyRight, playerSpeed) {
+    // x-axis move with player
+    if (keyIsDown(keyLeft)) { // LEFT
+      this.itemX += playerSpeed;
+    }
+
+    if (keyIsDown(keyRight)) { // RIGHT
+      this.itemX -= playerSpeed;
+    }
+  }
+
+  moveWithPlayerY(keyUp, keyDown, playerSpeed) {
+    // y-axis move with player
+    if (keyIsDown(keyUp)) { // UP
+      this.itemY += playerSpeed;
+    }
+
+    if (keyIsDown(keyDown)) { // DOWN
+      this.itemY -= playerSpeed;
+    }
+  }
+
+  itemShow(spriteW, spriteH) {
+    image(this.img, this.itemX + width/2, this.itemY + height/2, spriteW, spriteH*0.25);
+  }
+
+  mapping(
+    worldW, worldH,
+    minimapX, minimapY,
+    minimapW, minimapH,
+    dotSize, colors) {
+    // minimap boundries
+    let minimapXMin = minimapX - minimapW/2 + dotSize/2;
+    let minimapXMax = minimapX + minimapW/2 - dotSize/2;
+
+    let minimapYMin = minimapY - minimapH/2 + dotSize/2;
+    let minimapYMax = minimapY + minimapH/2 - dotSize/2;
+
+    // dot
+    let mapX = map(this.mapX, -worldW/2, worldW/2, minimapXMin, minimapXMax);
+    let mapY = map(this.mapY, -worldH/2, worldH/2, minimapYMin, minimapYMax);
+
+    // items dot
+    fill(colors);
+    ellipse(mapX, mapY, dotSize);
+  }
+}
--- MajorProjectHomepage/MajorProjectV4.0/assets/moreSketches/Save+Load_Games.js
+++ MajorProjectHomepage/MajorProjectV4.0/assets/moreSketches/Save+Load_Games.js
@@ -23,11 +23,40 @@ function showSavedFiles() {
 function saveGame() {
   // save game
   let title = prompt("Enter Title", "");
-  let blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
+  // let blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
+  //
+  // if (title !== null) {
+  //   saveAs(blob, title + ".js");
+  // }
 
-  if (title !== null) {
-    saveAs(blob, title + ".js");
-  }
+  let saveVars = {};
+
+  saveVars.keyBinds = {};
+
+  // changable key binds
+  saveVars.keyBinds.toggleRanged = keyBindings.toggleRanged = 82; // r
+  saveVars.keyBinds = keyBindings.toggleMagic = 84; // t
+  saveVars.keyBinds = keyBindings.placeTrap = 81; // q
+  saveVars.keyBinds = keyBindings.up = 87; // w
+  saveVars.keyBinds = keyBindings.left = 65; // a
+  saveVars.keyBinds = keyBindings.down = 83; // s
+  saveVars.keyBinds = keyBindings.right = 68; // d
+  saveVars.keyBinds = keyBindings.interact = 70; // f
+  saveVars.keyBinds = keyBindings.inventory = 69; // e
+  saveVars.keyBinds = keyBindings.openMap = 77; // m
+  saveVars.keyBinds = keyBindArray = [ // displayed bindings
+                              keyBindings.toggleRanged,
+                              keyBindings.toggleMagic,
+                              keyBindings.placeTrap,
+                              keyBindings.up,
+                              keyBindings.left,
+                              keyBindings.down,
+                              keyBindings.right,
+                              keyBindings.interact,
+                              keyBindings.inventory,
+                              keyBindings.openMap];
+
+
 }
 
 //------------------------------------------------------------------------------
