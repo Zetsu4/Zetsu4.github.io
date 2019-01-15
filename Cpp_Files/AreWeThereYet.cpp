85212d6df0a28525cf240859e4b0445d1931a18c
Zetsu4 <42389794+Zetsu4@users.noreply.github.com>
2019-01-15 14:12:01 -0600

    Update AreWeThereYet.cpp

--- Cpp_Files/AreWeThereYet.cpp
+++ Cpp_Files/AreWeThereYet.cpp
@@ -8,5 +8,26 @@ using namespace std;
 
 int main()
 {
+    int n1, n2, n3, n4;
+    cin >> n1;
+    cin >> n2;
+    cin >> n3;
+    cin >> n4;
 
+    int numbers[4] = {n1, n2, n3, n4};
+
+    for (int i=0; i < 5; i++)
+    {
+        string dists = "";
+
+        for (int j = 0; j < 4; j++)
+        {
+            
+            dists +=  + " "
+        }
+
+        cout << 
+    }
+
+    return 0;
 }
