575f09cf9ac36b3bf47446edad1aad149fedf243
Zetsu4 <42389794+Zetsu4@users.noreply.github.com>
2019-01-16 09:53:13 -0600

    started Sunflowers CCC problem

--- Cpp_Files/AreWeThereYet.cpp
+++ Cpp_Files/AreWeThereYet.cpp
@@ -8,25 +8,32 @@ using namespace std;
 
 int main()
 {
-    int numbers[3];
+    int numbers[4];
     cin >> numbers[0];
     cin >> numbers[1];
     cin >> numbers[2];
     cin >> numbers[3];
 
 
-    // five lines
-    for (int k=0; k < 5; k++)
-    {
-        // one line
-        for (int i=0; i < 5; i++)
-        {
-            int runningTotal = 0;
+    for (int k=0; k < 5; k++) {
+        // five lines
 
-            for (int j=0; j < i; j++)
-            {
-                // adding distances
-                runningTotal = runningTotal + numbers[j];
+        for (int i=0; i < 5; i++) {
+            // one line
+            int runningTotal = 0;
+            int loops;
+
+            if (k>i) {
+                // pos 1 > pos 2
+                for (loops = i; loops < k; loops++) {
+                    runningTotal += numbers[loops];
+                }
+
+            } else if (k<i) {
+                // pos 1 < pos 2
+                for (loops = k; loops < i; loops++) {
+                    runningTotal += numbers[loops];
+                }
             }
 
             cout << runningTotal << " ";
---
+++ Cpp_Files/Sunflowers.cpp
@@ -0,0 +1,64 @@
+// Sunflowers
+// Travis Ahern
+// Jan. 16, 2019
+
+#include <iostream>
+
+using namespace std;
+
+int rotate90clock(anArr, arrSqr) {
+    // rotate array 90 degrees clockwise
+    int newArr[arrSqr*arrSqr];
+
+    for (int i = 0; i < arrSqr*arrSqr; i++)
+    {
+        newArr[i] = anArr[];
+    }
+
+    return newArr;
+}
+
+int arrMin(anArr, arrLen) {
+    // find smallest value in array
+    int smallestValue = anArr[0];
+    for (int i = 0; i < arrLen; i++)
+    {
+        if (anArr[i] < smallestValue)
+        {
+            smallestValue = anArr[i]
+        }
+    }
+
+    return smallestValue;
+}
+
+int main()
+{
+    int numOfSunflowers;
+    cin >> numOfSunflowers;
+
+    // create numArray
+    int numArray[numOfSunflowers * numOfSunflowers];
+    for (int i = 0; i < numOfSunflowers * numOfSunflowers; i++)
+    {
+        cin >> numArray[i];
+    }
+
+    // rotate array
+    int arrayMin = arrMin(numArray, numOfSunflowers * numOfSunflowers);
+    while (numArray[i] != arrayMin)
+    {
+        numArray = rotate90clock(numArray, numOfSunflowers);
+    }
+
+    // output numArray
+    for (int i = 0; i < numOfSunflowers * numOfSunflowers; i++)
+    {
+        if (i % numOfSunflowers) 
+        {
+            cout << endl;
+        }
+
+        cout << numArray[i] + ' ';
+    }
+}
