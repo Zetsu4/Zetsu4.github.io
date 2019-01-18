e2214353fd350268ea0965a0d1f145b76279b5c6
Zetsu4 <42389794+Zetsu4@users.noreply.github.com>
2019-01-18 09:52:58 -0600

    problems

--- Cpp_Files/ChooseYourOwnpath.cpp
+++ Cpp_Files/ChooseYourOwnpath.cpp
@@ -6,7 +6,35 @@
 
 using namespace std;
 
+int pages;
+
+int findElement(index)
+{
+  pageChoices[index] == index;
+}
+
 int main()
 {
+  int pages;
+  cin >> pages;
 
+  // get input
+  pageChoices[pages];
+  for (int i = 0; i < pages; i++) 
+    cin >> pageChoice[i];
+
+  char pagesAccessible = 'Y';
+  for (int i = 0; i < pages; i++) 
+  {
+    if (i )
+    {
+      pagesAccessible = 'N';
+      break;
+    }
+  }
+
+  // output
+  cout << pagesAccessible << endl;
+  cout << shortestPath << endl;
+  return 0;
 }
--- Cpp_Files/Sunflowers.cpp
+++ Cpp_Files/Sunflowers.cpp
@@ -6,50 +6,65 @@
 
 using namespace std;
 
-int rotate90clock(int anArr, int arrSqr) {
+int numOfSunflowers;
+
+// create number array
+int **numArray;
+
+int **rotate90clock() {
   // rotate array 90 degrees clockwise
-  int newArr[arrSqr][arrSqr];
+  int **newArr = new int *[numOfSunflowers];
+  for (int i = 0; i < numOfSunflowers; i++)
+    newArr[i] = new int[numOfSunflowers];
 
-  for (int i = 0; i < arrSqr; i++)
-    for (int j = 0; j < arrSqr; j++)
-      newArr[i][j] = anArr[j][i];
+  for (int i = 0; i < numOfSunflowers; i++)
+    for (int j = 0; j < numOfSunflowers; j++)
+      newArr[i][j] = numArray[j][i];
 
-  return **newArr;
+  return newArr;
 }
 
-int arrMin(int anArr, int arrSqr) {
+int arrMin() {
   // find smallest value in array
-  int smallestValue = anArr[0][0];
-  for (int i = 0; i < arrSqr; i++)
-    for (int j = 0; j < arrSqr; j++)
-      if (anArr[i][j] < smallestValue)
-        smallestValue = anArr[i][j];
+  int smallestValue = numArray[0][0];
+  for (int i = 0; i < numOfSunflowers; i++)
+    for (int j = 0; j < numOfSunflowers; j++)
+      if (numArray[i][j] < smallestValue)
+        smallestValue = numArray[i][j];
 
   return smallestValue;
 }
 
 int main()
 {
-  int numOfSunflowers;
   cin >> numOfSunflowers;
 
-  // create numArray
-  int numArray[numOfSunflowers][numOfSunflowers];
+  // create number array
+  numArray = new int *[numOfSunflowers];
   for (int i = 0; i < numOfSunflowers; i++)
-    for (int j = 0; j < numOfSunflowers; i++)
+      numArray[i] = new int[numOfSunflowers];
+  // int **numArray = new int *[numOfSunflowers];
+  // for (int i = 0; i < numOfSunflowers; i++)
+  //   numArray[i] = new int[numOfSunflowers];
+
+  // put input in number array
+  for (int i = 0; i < numOfSunflowers; i++)
+    for (int j = 0; j < numOfSunflowers; j++)
       cin >> numArray[i][j];
 
   // rotate array
-  int arrayMin = arrMin(**numArray, numOfSunflowers);
+  int arrayMin = arrMin();
   while (numArray[0][0] != arrayMin)
-    numArray = rotate90clock(**numArray, numOfSunflowers);
+    numArray = rotate90clock();
 
   // output numArray
-  for (int i = 0; i < numArray.length(); i++)
+  for (int i = 0; i < numOfSunflowers; i++)
   {
-    for (int j = 0; j < numArray[i].length(); j++)
-      cout << numArray[i][j] + ' ';
+    for (int j = 0; j < numOfSunflowers; j++)
+      cout << numArray[i][j] << " ";
 
     cout << endl;
   }
+
+  return 0;
 }
