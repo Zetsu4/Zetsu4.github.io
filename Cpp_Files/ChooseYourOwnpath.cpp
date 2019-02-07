66c8f7a2c4477d03584faf64c25fbd8ad21e559c
Zetsu4 <42389794+Zetsu4@users.noreply.github.com>
2019-02-07 15:15:01 -0600

    Update ChooseYourOwnpath.cpp

--- Cpp_Files/ChooseYourOwnpath.cpp
+++ Cpp_Files/ChooseYourOwnpath.cpp
@@ -6,35 +6,59 @@
 
 using namespace std;
 
-int pages;
+int **allChoices;
 
-int findElement(index)
+bool isEnding(item)
 {
-  pageChoices[index] == index;
+  return item == 0;
+}
+
+int goThroughChoices(int pos, int iteration)
+{
+  if (allChoices[pos][0] == 0)
+    return iteration;
+  else
+    return goThroughChoices(allChoices[pos][1], iteration++);
+}
+
+int findShortestPath(int **arr, int length)
+{
+  int page;
+  
+  while (!isEnding(page))
+  {
+    if ()
+  }
 }
 
 int main()
 {
   int pages;
+  int shortestPath;
+  bool pagesReachable = false;
+
   cin >> pages;
 
-  // get input
-  pageChoices[pages];
-  for (int i = 0; i < pages; i++) 
-    cin >> pageChoice[i];
 
-  char pagesAccessible = 'Y';
-  for (int i = 0; i < pages; i++) 
+  // get input
+  **allChoices = new int *[pages];
+  for (int i = 0; i < pages; i++)
   {
-    if ()
-    {
-      pagesAccessible = 'N';
-      break;
-    }
+    int num;
+    cin >> num;
+
+    allChoices[i] = new int[num];
+    allChoices[i][0] = num;
+
+    for (int j = 0; j < allChoices[i][0]; j++)
+      cin >> allChoices[i][j];
   }
 
+  // find shortest path
+  int path = goThroughChoices(0, 0);
+  cout << path << endl;
+
   // output
-  cout << pagesAccessible << endl;
-  cout << shortestPath << endl;
+  // cout << shortestPath << endl;
   return 0;
 }
