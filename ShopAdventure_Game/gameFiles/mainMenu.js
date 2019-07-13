56e92a2f655030c3d89cdc46f088068c92cdfb1e
Zetsu4 <42389794+Zetsu4@users.noreply.github.com>
2019-07-12 18:06:45 -0600

    worked on Shop Adventure game

--- ShopAdventure_Game/gameFiles/mainMenu.js
+++ ShopAdventure_Game/gameFiles/mainMenu.js
@@ -1,4 +1,22 @@
 // game loop
 function draw() {
+    if (gameState === "Main Menu") // main menu
+        mainMenu();
 
+    else if (gameState === "In Game") // play game
+        inGame();
+
+    else if (gameState === "Settings") // setting menu
+        settingsMenu();
+}
+
+function mainMenu() {
+    
+}
+
+function inGame() {
+    if (shopOpen)
+        shopScreen();
+    else 
+        adventureScreen();
 }
--- ShopAdventure_Game/loadingFile.js
+++ ShopAdventure_Game/loadingFile.js
@@ -3,12 +3,17 @@
 // Jul. 11, 2019
 
 // vars
+let shopImg = {};
+
+let gameState;
 
 
 function preload() {
-
+    shopImg.background = loadImage("assets/img/shop/shopBackground.png");
 }
 
 function setup() {
-    
+    createCanvas(windowWidth, windowHeight);
+
+    gameState = "Main Menu";
 }
